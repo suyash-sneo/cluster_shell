8259c346342975aaa74af26f050f8fe8cac04fe8
random1001guy <35926701+random1001guy@users.noreply.github.com>
2019-10-08 11:07:50 +0530

    create a pipe between 2 cmds with file i/o

--- pipe.c
+++ pipe.c
@@ -1,29 +1,75 @@
-#include "header.h"
+##include <stdio.h>
+#include <unistd.h>                     
+#include <stdlib.h>                     
+#include <signal.h>                     
+#include <time.h>						
+#include <stdint.h> 					
+#include <sys/time.h>					
+#include <sys/wait.h>
+#include <pthread.h>
+#include <sys/select.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
-int main(int argc, char *argv[]){
 
-		int pipefd[2];
-		pipe(pipefd);
+void pipeit(char **incmd, int infd, int outfd, char **outcmd);
 
 
-		int pid=fork();
+ int main(int argc,char *argv){
 
-		if(pid==0){
-			close(pipefd[0]);
-			dup2(pipefd[1],1);
 
-			int err2;
-			if( (err2 = execlp(argv[1],argv[1],(char *)0)) == -1)
-			perror("exec: ");
+ 	int infd;
+ 	if((infd=open("i.txt",O_RDONLY))==-1)
+ 		perror("open infd: ");
+ 	int outfd;
+ 	if((outfd=open("h.txt",O_WRONLY | O_CREAT))==-1)
+ 		perror("open outfd: ");
+ 	//outfd=-1;
+ 	//infd=-1;
+ 	char *incmd[]={"wc",(char *)0};
+ 	char *outcmd[]={"wc",(char *)0};
+ 	pipeit(incmd,infd,outfd,outcmd);
+ 	printf("main done\n");
+}
+
+
+void pipeit(char **incmd, int infd, int outfd, char **outcmd){
+
+	int pipefd[2];
+	int status;
+	int pid;
+
+	if (pipe(pipefd) == -1) perror("pipe : ");
+
+	printf("pipefd = %d %d\n", pipefd[0],pipefd[1]);
+
+	if((pid=fork())==0){
+		setpgid(0,0);
+		close(pipefd[0]);
+		if(infd !=-1){
+			dup2(infd,STDIN_FILENO);
 		}
-		else{
-			close(pipefd[1]);
-			int err1,err2;
-			if((err1=dup2(pipefd[0],0))==-1)
-				perror("dup2: ");
-			int status;
-			wait(&status);
-			if( (err2 = execlp(argv[2],argv[2],(char *)0)) == -1)
-				perror("exec: ");
+		dup2(pipefd[1],STDOUT_FILENO);
+		execvp(incmd[0],incmd);
+	}
+
+	waitpid(-1,&status,WNOHANG);
+	printf("pid :%d\tstatus :%d\n",pid,status);
+
+	if((pid=fork())==0){
+		setpgid(0,0);
+		close(pipefd[1]);
+		if(outfd !=-1){
+			dup2(outfd,STDOUT_FILENO);
 		}
+		dup2(pipefd[0],STDIN_FILENO);
+		execvp(outcmd[0],outcmd);
+	}
+
+	waitpid(-1,&status,WNOHANG);
+	printf("pid :%d\tstatus :%d\n",pid,status);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	wait(&status);
 }
